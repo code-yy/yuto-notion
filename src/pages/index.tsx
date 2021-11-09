6066da8bbe6de164c7a9682ec4ddbf19baff0333
ユウト <jiyeyouren523@gmail.com>
2021-11-09 22:32:52 +0900

    🎉 Begin a propject

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className="text-red-400">hello</div>
+      <div className="text-blue-400">hello</div>
+    </div>
+  );
+};
+
+export default Home;
