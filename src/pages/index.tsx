60fa62cbf583aff872e821d64a994889b1f71d25
ユウト <jiyeyouren523@gmail.com>
2021-11-10 16:06:47 +0900

    🎉 Begin a Project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,34 @@
 import type { NextPage } from 'next';
+import { Client } from '@notionhq/client';
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
+const databaseId: any = process.env.NOTION_DATABASE_ID;
+
+async function addItem(text: any) {
+  try {
+    const response: any = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log('Success! Entry added.');
+  } catch (error: any) {
+    console.error(error.body);
+  }
+}
+
+addItem('Yurts in Big Sur, California');
 
 const Home: NextPage = () => {
   return (
